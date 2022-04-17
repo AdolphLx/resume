0b44f23f095f3bf8d371da254a6521dd0719fe71
liuxiang <784056810@qq.com>
2022-04-17 23:34:46 +0800

    新增静态文件夹

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
+// const { init: initDB, Counter } = require("./db");
 
 const logger = morgan("tiny");
 
@@ -12,47 +12,28 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
+const { resolve } = require('path');
+const publucPath = resolve('./www');
+app.use(express.static(publucPath)); //托管静态文件，能通过http方式访问
+
 // 首页
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
-});
-
 // 获取计数
 app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
+  // const result = await Counter.count();
   res.send({
     code: 0,
-    data: result,
+    data: [],
   });
 });
 
-// 小程序调用，获取微信 Open ID
-app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
-  }
-});
+const port = process.env.PORT || 8080;
 
-const port = process.env.PORT || 80;
-
-async function bootstrap() {
-  await initDB();
+async function bootstrap () {
+  // await initDB();
   app.listen(port, () => {
     console.log("启动成功", port);
   });
